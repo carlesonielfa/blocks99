d4158f395bee80de2d77baef717fae943ac41d3a
Carles Onielfa <carlesop@nuclia.com>
2024-05-26 19:02:30 +0200

    WIP: Feature Flags

---
+++ src/utils/flags.js
@@ -0,0 +1,23 @@
+const FLAGS_URI = "https://raw.githubusercontent.com/carlesonielfa/blocks99/dc586ebc9f0171139713d07ea9626f9f8579dac7/feature_flags.json"
+let flags_loaded = false;
+async function loadFlags() {
+    try {
+        let response = await fetch(FLAGS_URI);
+        let flags = await response.json();
+        flags_loaded = true;
+        return flags;
+    } catch (err) {
+        flags_loaded = false;
+        console.error("Error loading feature flags", err);
+        return null;
+    }
+}
+
+let flags = await loadFlags();
+
+export default async function hasFlag(flag) {
+    if (!flags_loaded) {
+        // Try to load the flags again
+        flags = await loadFlags();
+    }
+}
